f60f7c9e40b5d892a8d2dcebfd94d0b1cefc625b
rajbharati27 <rajbharati@dsmartsys.in>
2024-08-24 03:22:21 -0500

    project setup backend

---
+++ Server/src/index.ts
@@ -0,0 +1,13 @@
+import express, { Application, Request, Response } from "express";
+import "dotenv/config";
+
+const app: Application = express();
+const PORT = process.env.PORT || 7000;
+
+app.get('/',(req:Request,res:Response)=>{
+    return res.send("Hiii Raj");
+})
+
+app.listen( PORT, ()=>{
+    console.log( `Server started at port ${PORT}`);   
+})
