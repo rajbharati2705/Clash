5261e2c9a7055c11574d0fd6796ca527517a06b1
rajbharati27 <rajbharati@dsmartsys.in>
2024-08-24 04:01:35 -0500

    frontend steup done

---
+++ front/src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <div className='text-red-500'>Home Page</div>
+      <Button>HII Raj</Button>
+    </div>
+    
+  )
+}
+
+export default App
